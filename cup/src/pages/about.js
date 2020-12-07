790a10b7248acd5515ebbe64943ff48f4eb51acf
Jo Halverson <johalverson@Jos-MacBook-Pro.attlocal.net>
2020-12-06 22:11:30 -0600

    final final

--- cup/src/pages/about.js
+++ cup/src/pages/about.js
@@ -11,6 +11,11 @@ const About = () => {
             <h2>Everything You Never Needed To Know About Jo</h2>
             <p> 
                 So, as you know by now, I'm Jo. I'm 23, and live in Dallas. 
+                I have far too much free time, now so why not start a blog?
+                Less cliche than a podcast thats for sure. 
+            </p>
+            <p>
+                
             </p>
             </div>
         </Layout>
--- cup/src/pages/contact.js
+++ cup/src/pages/contact.js
@@ -8,8 +8,11 @@ const Contact = () => {
             <Layout>
                 <SEO title="Contact"/>
                 <div style={{color:`maroon`, fontFamily:`monospace`}}>
-                <h1>Contact</h1>
-                <p>This is how you contact me.</p>
+                <h2>Contact</h2>
+                <p>If you have any issues with this site(DON'T), feel free to email or message me.
+                    If you have a topic you want to hear about, or perhaps a comment on one of my posts, feel free to email or message me.
+                </p>
+                <p>Instagram: @JoRegrets Email:jthalverson@att.net</p>
                 <Link to="/">Go Home</Link>
                 </div>
             </Layout>
